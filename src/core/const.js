832c5149f314b5e9f00846308a836a63b985bc12
Roscoe Hart <roscoewh@gmail.com>
2021-07-09 02:49:17 +1000

    Map and filter categories

--- src/core/const.js
+++ src/core/const.js
@@ -3,3 +3,7 @@ export const CATEGORY = {
   HARNESS: "harness",
   GREYHOUND: "greyhound",
 };
+
+export const CATEGORY_HORSE_ID = "4a2788f8-e825-4d36-9894-efd4baf1cfae";
+export const CATEGORY_GREYHOUND_ID = "9daef0d7-bf3c-4f50-921d-8e818c60fe61";
+export const CATEGORY_HARNESS_ID = "161d9be2-e909-4326-8c2c-35ed71fb460b";
--- src/core/data.js
+++ src/core/data.js
@@ -1,10 +1,32 @@
 import axios from "axios";
 
+import {
+  CATEGORY,
+  CATEGORY_HORSE_ID,
+  CATEGORY_HARNESS_ID,
+  CATEGORY_GREYHOUND_ID,
+} from "../core/const";
+
+function mapCategory(categoryId) {
+  switch (categoryId) {
+    case CATEGORY_GREYHOUND_ID:
+      return CATEGORY.GREYHOUND;
+    case CATEGORY_HARNESS_ID:
+      return CATEGORY.HARNESS;
+    case CATEGORY_HORSE_ID:
+      return CATEGORY.HORSE;
+    default:
+      return null;
+  }
+}
+
 function mapRaces([, race]) {
+  console.log(race);
   return {
     meetingName: race.meeting_name,
     raceNumber: race.race_number,
     advertisedStart: race.advertised_start.seconds,
+    category: mapCategory(race.category_id),
   };
 }
 
@@ -14,7 +36,10 @@ function compareRaces(a, b) {
 
 function transformResponse(response) {
   const races = Object.entries(response.data.data.race_summaries);
-  return races.map(mapRaces).sort(compareRaces);
+  return races
+    .map(mapRaces)
+    .filter(({ category }) => category !== null)
+    .sort(compareRaces);
 }
 
 export function fetchRaces() {
