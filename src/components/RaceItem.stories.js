d3a701659e2b0420830fdb621f619c43cd30e3b6
Roscoe Hart <roscoewh@gmail.com>
2021-07-08 23:43:10 +1000

    Update RaceItem styles

--- src/components/RaceItem.stories.js
+++ src/components/RaceItem.stories.js
@@ -17,4 +17,33 @@ export const Default = Template.bind({});
 Default.args = {
   meetingName: "Capalba",
   raceNumber: 6,
+  secondsRemaining: 5 * 60,
+};
+
+export const UnderFiveMinutes = Template.bind({});
+UnderFiveMinutes.args = {
+  meetingName: "Capalba",
+  raceNumber: 6,
+  secondsRemaining: 4 * 60 + 22,
+};
+
+export const UnderThreeMinutes = Template.bind({});
+UnderThreeMinutes.args = {
+  meetingName: "Capalba",
+  raceNumber: 6,
+  secondsRemaining: 2 * 60 + 57,
+};
+
+export const UnderOneMinute = Template.bind({});
+UnderOneMinute.args = {
+  meetingName: "Capalba",
+  raceNumber: 6,
+  secondsRemaining: 33,
+};
+
+export const Overdue = Template.bind({});
+Overdue.args = {
+  meetingName: "Capalba",
+  raceNumber: 6,
+  secondsRemaining: -99,
 };
--- src/components/RaceItem.vue
+++ src/components/RaceItem.vue
@@ -4,6 +4,12 @@
       {{ meetingName.toUpperCase() }}
       <span class="race-number">R{{ raceNumber }}</span>
     </div>
+    <div
+      class="time-remaining"
+      v-bind:style="{ color: `var(--color-time-${timeColorIndex})` }"
+    >
+      {{ timeRemaining }}
+    </div>
   </div>
 </template>
 
@@ -19,17 +25,50 @@ export default {
       type: Number,
       required: true,
     },
+    secondsRemaining: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    minutesRemaining: function () {
+      return Math.floor(this.secondsRemaining / 60);
+    },
+    timeRemaining: function () {
+      const seconds = this.secondsRemaining % 60;
+      return `${
+        this.minutesRemaining ? `${this.minutesRemaining}m ` : ""
+      }${seconds}s`;
+    },
+    timeColorIndex: function () {
+      if (this.minutesRemaining < 0) return 0;
+      if (this.minutesRemaining < 1) return 1;
+      if (this.minutesRemaining < 3) return 2;
+      if (this.minutesRemaining < 5) return 3;
+      return "hover";
+    },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .race-item {
+  display: flex;
+  justify-content: space-between;
   color: var(--color-text);
+  padding: var(--space-2) var(--space-1);
+  cursor: pointer;
+}
+
+.race-item:hover {
+  background-color: var(--color-hover);
 }
 
 .race-number {
-  color: var(--color-primary);
+  font-style: italic;
+}
+
+.time-remaining {
+  font-weight: bold;
 }
 </style>
